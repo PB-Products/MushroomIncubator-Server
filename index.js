edc6d187105ae5dac7806494ceaaedef2037a1cf
Piumi96 <piumidp96@gmail.com>
2020-02-27 14:00:16 +0530

    set database connection for mqtt server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var con = require('./db');
 var writeData = require('./data_handler');
 
 var options = keys.options;
-const client = mqtt.connect('mqtt://tailor.cloudmqtt.com', options);
+const client = mqtt.connect('mqtt://soldier.cloudmqtt.com', options);
 
 con.connect((err) => {
     if (err) throw err;
@@ -18,8 +18,30 @@ client.on('connect', () => {
 
 client.on('message', (topic, message) => {
     if (topic === 'Data') {
-        console.log(JSON.parse(message.toString()));
-        var data = JSON.parse(message.toString());
-        writeData(data);
+        //console.log(JSON.parse(message.toString()));
+        //var data = JSON.parse(message.toString());
+        //writeData(data);
+        if (message.toString() !== "Data not received") {
+            var data = message.toString().split(':-')[1].split(',');
+
+            console.log(data)
+
+            var map = {
+                temp: data[0],
+                hum: data[1],
+                light: data[2],
+                soil: data[3]
+            }
+
+            console.log(map);
+
+            try {
+                writeData(map);
+                //sms.validateSms(map);
+            } catch (err) {
+                console.log(err);
+            }
+
+        }
     }
 })
